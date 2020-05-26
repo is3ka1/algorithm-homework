275b1cf1047916b50486e7ab56c72cc9a2bef56f
isekai <brian1689878638@gmail.com>
2020-05-26 14:29:45 +0800

    Put accept csv files in the data dir and output graphs into graph dir

--- plot_graphs.py
+++ plot_graphs.py
@@ -4,12 +4,13 @@ import pandas as pd
 import seaborn as sns
 
 
-algos = ["exchange_sort", "merge_sort", "quick_sort", "counting_sort", "heap_sort"]
+data_dir = './data'
+graph_dir = './graph'
 data_dist_cats = ['ascending', 'descending', 'random']
 
 def plot_algo(algo):
     # Plot the standard deviation line
-    df = pd.read_csv(f'./{algo}.csv', names=['n', 'ascending', 'descending', 'random'])
+    df = pd.read_csv(os.path.join(data_dir, f'{algo}.csv'), names=['n', *data_dist_cats])
     sns_df = pd.concat(
         [
             pd.DataFrame({
@@ -23,11 +24,17 @@ def plot_algo(algo):
     )
     sns_plot = sns.relplot(x='n', y='second', hue='data_dist', kind='line', ci="sd", data=sns_df)
     sns_plot.fig.suptitle(algo)
-    if not os.path.isdir('./graph'):
-        os.mkdir('./graph')
-    sns_plot.savefig(f'./graph/{algo}.png', dpi=600)
+    if not os.path.isdir(graph_dir):
+        os.mkdir(graph_dir)
+    sns_plot.savefig(os.path.join(graph_dir, f'{algo}.png'), dpi=600)
     print(f'{algo} graph saved!')
 
 if __name__ == "__main__":
-    for algo in algos:
-        plot_algo(algo)
+    if not os.path.isdir(data_dir):
+        print('You should run measure.sh first!')
+        exit()
+
+    for file_name in os.listdir(data_dir):
+        path = os.path.join(data_dir, file_name)
+        if os.path.isfile(path) and path.endswith('.csv'):
+            plot_algo(file_name[:-4])
